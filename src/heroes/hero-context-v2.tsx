096b90ae00128642163be96f902b6ec77677ef97
Devlin D <webmasterdevlin>
2020-10-03 10:09:28 +0200

    updated

--- src/heroes/hero-context-v2.tsx
+++ src/heroes/hero-context-v2.tsx
@@ -69,7 +69,7 @@ export const HeroContextV2 = () => {
       store.setError("");
       store.isLoading = true;
       try {
-        await deleteHero(); // check
+        await deleteHero(id);
         store.heroes = store.heroes.filter((h) => h.id !== id);
       } catch (e) {
         store.setError(e);
